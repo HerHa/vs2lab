0b08709c5ea124de3c65e5a63b8347f655a78b2f
Rebecca Laskowski <167071449+b-ec@users.noreply.github.com>
2026-04-15 17:45:11 +0200

    lab 1 versuch
     funktioniert noch nicht so ganz

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -16,6 +16,7 @@ class Server:
     """ The server """
     _logger = logging.getLogger("vs2lab.lab1.clientserver.Server")
     _serving = True
+    _database = {"Jamie": 42, "Hannah": 37, "Bec": 55}
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,7 +36,25 @@ class Server:
                     data = connection.recv(1024)  # receive data from client
                     if not data:
                         break  # stop if client stopped
-                    connection.send(data + "*".encode('ascii'))  # return sent data plus an "*"
+
+                    message = data.decode('ascii')
+                    
+                    if message.startswith("GET "):
+                        name = message[4:]  # Extract name after "GET "
+                        self._logger.info("Requested Phone Number for " + name)
+                        if name in self._database:
+                            response = str(self._database[name])  # Get the number
+                        else:
+                            response = "No entry for " + name 
+                        connection.send(response.encode('ascii'))
+                    elif message == "GETALL":
+                        self._logger.info("Requested All Phone Numbers")
+                        response = str(self._database)
+                        connection.send(response.encode('ascii'))
+                    else:
+                        self._logger.info("Defaulting to echo for message: " + message)
+                        connection.send(data + "*".encode('ascii'))  # Default echo
+
                 connection.close()  # close the connection
             except socket.timeout:
                 pass  # ignore timeouts
@@ -52,16 +71,35 @@ class Client:
         self.sock.connect((const_cs.HOST, const_cs.PORT))
         self.logger.info("Client connected to socket " + str(self.sock))
 
-    def call(self, msg_in="Hello, world"):
+    def call(self, msg_in):
         """ Call server """
         self.sock.send(msg_in.encode('ascii'))  # send encoded string as data
         data = self.sock.recv(1024)  # receive the response
         msg_out = data.decode('ascii')
         print(msg_out)  # print the result
-        self.sock.close()  # close the connection
-        self.logger.info("Client down.")
         return msg_out
+    
+    def get(self, name):
+        """Get a specific item by name"""
+        request = f"GET {name}"  # Create request message
+        self.sock.send(request.encode('ascii'))  # Send to server
+        self.logger.info("Requested Phone Number for " + name)
+        data = self.sock.recv(1024)  # Receive response
+        response = data.decode('ascii')
+        print(response)
+        return response
+
+    def getAll(self):
+        """Get all items"""
+        request = "GETALL"  # Simple request
+        self.sock.send(request.encode('ascii'))  # Send to server
+        self.logger.info("Requested All Phone Numbers")
+        data = self.sock.recv(1024)  # Receive response
+        response = data.decode('ascii')
+        print(response)
+        return response
 
     def close(self):
         """ Close socket """
         self.sock.close()
+        self.logger.info("Client down.")
